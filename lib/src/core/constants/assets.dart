eb2c81984d8a6efee6d84e43e8f8520402ecacf6
Caths.Dev <Caths.Dev@gmail.com>
2025-10-30 12:37:01 +0100

    feat: Add assets constants

---
+++ lib/src/core/constants/assets.dart
@@ -0,0 +1,13 @@
+abstract final class AssetsPath {
+  static const defaultImageAsset = 'assets/images/header/default.png';
+  static const areas = 'assets/images/header/areas.png';
+  static const cleaning = 'assets/images/header/cleaning.png';
+  static const emergency = 'assets/images/header/emergency.png';
+  static const focus = 'assets/images/header/focus.png';
+  static const mode = 'assets/images/header/mode.png';
+  static const mood = 'assets/images/header/mood.png';
+  static const rubberduck = 'assets/images/header/rubberduckcat.png';
+  static const welcome = 'assets/images/header/welcome.png';
+  static const areaCard = 'assets/images/cards/area.png';
+  static const taskCard = 'assets/images/cards/tasks.png';
+}
