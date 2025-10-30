1a97b7c003e38b412ef2e6a3b03623263d3745a6
Caths.Dev <Caths.Dev@gmail.com>
2025-10-30 18:27:25 +0100

    feat: Add UserEntity for auth domain

---
+++ lib/src/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,17 @@
+class UserEntity {
+  final String id;
+  final String email;
+  final DateTime createdAt;
+  const UserEntity({
+    required this.id,
+    required this.email,
+    required this.createdAt,
+  });
+  UserEntity copyWith({String? id, String? email, DateTime? createdAt}) {
+    return UserEntity(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+}
