529a80138715d51a4270cafece1c93e3d4e16cff
Caths.Dev <Caths.Dev@gmail.com>
2025-10-31 11:51:53 +0100

    feat: Add SignInUseCase

--- lib/src/features/auth/domain/usecases/sign_in_usecase.dart
+++ lib/src/features/auth/domain/usecases/sign_in_usecase.dart
@@ -1,1 +1,13 @@
-// todo
+import 'package:bd_app_v0/src/core/error/failures.dart';
+import 'package:bd_app_v0/src/features/auth/domain/entities/user_entity.dart';
+import 'package:bd_app_v0/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class SignInUseCase {
+  final AuthRepository repository;
+  const SignInUseCase(this.repository);
+
+  Future<Either<Failures, UserEntity>> call(String email, String password) {
+    return repository.signInWithEmailAndPassword(email, password);
+  }
+}
