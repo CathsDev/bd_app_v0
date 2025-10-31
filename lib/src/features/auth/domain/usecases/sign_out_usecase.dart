c0792521fee228c06b18020a1648cee97bb98fc1
Caths.Dev <Caths.Dev@gmail.com>
2025-10-31 11:59:27 +0100

    feat: Add SignOutUseCase

--- lib/src/features/auth/domain/usecases/sign_out_usecase.dart
+++ lib/src/features/auth/domain/usecases/sign_out_usecase.dart
@@ -1,1 +1,12 @@
-// todo
+import 'package:bd_app_v0/src/core/error/failures.dart';
+import 'package:bd_app_v0/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class SignOutUseCase {
+  final AuthRepository repository;
+  const SignOutUseCase(this.repository);
+
+  Future<Either<Failures, void>> call() {
+    return repository.signOut();
+  }
+}
