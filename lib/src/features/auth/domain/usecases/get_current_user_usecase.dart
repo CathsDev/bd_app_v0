fa7109961c91abf05c3601eabfea24008a2d16e8
Caths.Dev <Caths.Dev@gmail.com>
2025-10-31 12:18:51 +0100

    feat: Add GetCurrentUserUseCase

--- lib/src/features/auth/domain/usecases/get_current_user_usecase.dart
+++ lib/src/features/auth/domain/usecases/get_current_user_usecase.dart
@@ -1,1 +1,13 @@
-// todo
+import 'package:bd_app_v0/src/core/error/failures.dart';
+import 'package:bd_app_v0/src/features/auth/domain/entities/user_entity.dart';
+import 'package:bd_app_v0/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class GetCurrentUserUseCase {
+  final AuthRepository repository;
+  const GetCurrentUserUseCase(this.repository);
+
+  Future<Either<Failures, UserEntity>> call() {
+    return repository.getCurrentUser();
+  }
+}
