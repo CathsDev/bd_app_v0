49f5cbcf24e69f5445faa4189b8eb7500fe520c9
Caths.Dev <Caths.Dev@gmail.com>
2025-11-04 10:38:32 +0100

    feat(area): implement area selection logic and navigation

--- lib/src/features/area_select/presentation/area_select_screen.dart
+++ lib/src/features/area_select/presentation/area_select_screen.dart
@@ -1,14 +1,20 @@
 import 'package:bd_app_v0/src/core/constants/assets.dart';
+import 'package:bd_app_v0/src/core/routing/route_names.dart';
 import 'package:bd_app_v0/src/core/theme/text_styles.dart';
+import 'package:bd_app_v0/src/features/area_select/models/area_model.dart';
+import 'package:bd_app_v0/src/features/area_select/providers/area_select_provider.dart';
+import 'package:bd_app_v0/src/shared/widgets/cards/area_card.dart';
 import 'package:bd_app_v0/src/shared/widgets/header/header_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class AreaSelectScreen extends ConsumerWidget {
   const AreaSelectScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedId = ref.watch(areaSelectProvider).selectedId;
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -30,24 +36,24 @@ class AreaSelectScreen extends ConsumerWidget {
               ),
             ),
           ),
-          /* Expanded(
+          Expanded(
             child: ListView.separated(
               padding: const EdgeInsets.fromLTRB(8, 8, 8, 32),
               itemBuilder: (context, index) {
                 final area = areas[index];
-                return SelectCard(
-                  mode: area,
+                return AreaCard(
+                  model: area,
                   selected: area.id == selectedId,
                   onTap: () {
                     ref.read(areaSelectProvider.notifier).setSelected(area.id);
-                    //context.pushNamed(area.route);
+                    context.pushNamed(AppRoutes.taskTimer);
                   },
                 );
               },
               separatorBuilder: (_, _) => const SizedBox.shrink(),
               itemCount: areas.length,
             ),
-          ), */
+          ),
         ],
       ),
     );
