0a8c8a08ac801749d2f087d913784675bd9622ed
Caths.Dev <Caths.Dev@gmail.com>
2025-11-26 08:51:48 +0100

    feat (settings): add user controller

---
+++ lib/src/features/settings/state/user_controller.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:bd_app_v0/src/features/settings/domain/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserNotifier extends AsyncNotifier<User> {
+  @override
+  FutureOr<User> build() {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
