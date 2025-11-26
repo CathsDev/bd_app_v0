228d0cea7b09d89a84223b425b865be03c29e743
Caths.Dev <Caths.Dev@gmail.com>
2025-11-26 09:53:43 +0100

    feat (settings): add userNotifierProvider

--- lib/src/features/settings/state/user_provider.dart
+++ lib/src/features/settings/state/user_provider.dart
@@ -1,5 +1,7 @@
 import 'package:bd_app_v0/src/features/settings/data/user_repository.dart';
 import 'package:bd_app_v0/src/features/settings/data/user_repository_local.dart';
+import 'package:bd_app_v0/src/features/settings/domain/user.dart';
+import 'package:bd_app_v0/src/features/settings/state/user_controller.dart';
 import 'package:bd_app_v0/src/shared/state/areas/areas_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,3 +9,7 @@ final userRepositoryProvider = FutureProvider<UserRepository>((ref) async {
   final prefs = await ref.watch(sharedPreferencesProvider.future);
   return UserRepositoryLocal(preferences: prefs);
 });
+
+final userNotifierProvider = AsyncNotifierProvider<UserNotifier, User>(
+  UserNotifier.new,
+);
